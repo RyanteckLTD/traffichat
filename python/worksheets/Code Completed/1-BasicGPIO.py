57e99f93e40e78aa0521d999cee14275579bc537
Ryan Walmsley <Ryan@ryanteck.uk>
2015-05-04 12:49:11 +0100

    Stuff

---
+++ python/worksheets/Code Completed/1-BasicGPIO.py
@@ -0,0 +1,18 @@
+#TrafficHAT - Blink LED
+#By Your Name Here
+
+import RPi.GPIO as IO
+
+from time import sleep
+
+try:
+    while True:
+        IO.output(green,1)
+        
+except KeyboardInterrupt:
+    IO.cleanup()
+        
+except:
+    print "Error:", sys.exc_info()[0]
+    raise
+    
